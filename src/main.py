53ecb4db70a4a6dbbee862477f51e1bafe1275ac
Jacob0626 <jgonzalez8@caldwell.edu>
2026-04-29 23:45:46 +0000

    Apr 29, 2026, 7:45 PM

--- src/main.py
+++ src/main.py
@@ -3,5 +3,6 @@ import streamlit as st
 set.set_page_config(
     page_title = "Student Budget Tracker",
     Page_icon = "💰",
-    layout = "wide"
+    layout = "wide",
+    initial_sidebar_state = ""
 )
