b7c729d1d063cbc9e379925543b989c31a32400b
Jacob0626 <jgonzalez8@caldwell.edu>
2026-05-10 15:42:12 +0000

    May 10, 2026, 11:42 AM

--- src/main.py
+++ src/main.py
@@ -216,7 +216,7 @@ elif page == "Transactions":
     
     ifst.button("Save Edited Transactions")
     save_transactions(edited_df, "saved_transactions.csv")
-    st.s
+    st.success()
 
 
 elif page == "Budget Goals":
