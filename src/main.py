22197f564075ccfebe8f6c6977e41c79f09ab5c3
Jacob0626 <jgonzalez8@caldwell.edu>
2026-05-10 04:19:41 +0000

    May 10, 2026, 12:19 AM

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ def load_transactions(filename: str) -> pd.DataFrame:
     """Load transaction data from a CSV file and prepare it for analysis."""
     df = pd.read_cvs(get_data_path(filename))
     
-    df
+    df["date"] = pd.to_
 
 st.title("Student Budget & Spending Habit Tracker")
 
