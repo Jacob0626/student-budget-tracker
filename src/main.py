a0036cbacbd7354f9946b70fb899b8b8399c7fb5
Jacob0626 <jgonzalez8@caldwell.edu>
2026-05-10 05:00:43 +0000

    May 10, 2026, 1:00 AM

--- src/main.py
+++ src/main.py
@@ -53,7 +53,7 @@ def get_spending_by_category(df: pd.DataFrame) -> pd.DataFrame:
     expenses_df = df[df["type"] == "Expense"]
     
     category_summary = expenses_df.groupby("category", as_index=False)["amount"].sum()
-    category_summary = category_summary.sort_values("")
+    category_summary = category_summary.sort_values("amount", ascending=False)
 
 st.title("Student Budget & Spending Habit Tracker")
 
