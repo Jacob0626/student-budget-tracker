66f45f34e50cacf4c4478acdc4c0697cbd266271
Jacob0626 <jgonzalez8@caldwell.edu>
2026-05-10 03:56:42 +0000

    May 9, 2026, 11:56 PM

--- src/main.py
+++ src/main.py
@@ -34,4 +34,5 @@ elif page == "Transactions":
     st.header("Transactions")
     st.info("This section will let the user upload, view, and edit transaction data.")
 
-elif page == "Budget"
+elif page == "Budget Goals":
+    st.header("Budget Goals")
