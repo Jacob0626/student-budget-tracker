3342e7f325e0bb6341d576c29074e16a6ae71c74
Jacob0626 <jgonzalez8@caldwell.edu>
2026-05-10 04:21:02 +0000

    May 10, 2026, 12:21 AM

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ def load_transactions(filename: str) -> pd.DataFrame:
     df = pd.read_cvs(get_data_path(filename))
     
     df["date"] = pd.to_datetime(df["date"], errors="coerce")
-    df["amount"] = pd.to_numeric(df["amount"], errors=)
+    df["amount"] = pd.to_numeric(df["amount"], errors="coerce")
 
 st.title("Student Budget & Spending Habit Tracker")
 
