1724e01a1e2e017078e46f28ce28d63102b09fbb
Jacob0626 <jgonzalez8@caldwell.edu>
2026-05-10 14:54:54 +0000

    May 10, 2026, 10:54 AM

--- src/main.py
+++ src/main.py
@@ -85,7 +85,7 @@ Users will be able to upload transaction data, view summaries, analyze spending 
 st.divider()
 
 df = load_transactions("sample_transactions.csv")
-category_options = 
+category_options = ["All"] + sorted(df)
 
 # Sidebar navigation menu
 with st.sidebar:
