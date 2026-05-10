90ee2dcdca82e32ca57a536f58222cb46e67bbf7
Jacob0626 <jgonzalez8@caldwell.edu>
2026-05-10 05:19:59 +0000

    May 10, 2026, 1:19 AM

--- src/main.py
+++ src/main.py
@@ -71,7 +71,7 @@ def filter_transactions(df: pd.DataFrame, selected_type: str, selected_category:
         filtered_df = filtered_df[filtered_df["type"] == selected_type]
     
     if selected_category != "All":
-        fi
+        filtered_df = 
 
 
 st.title("Student Budget & Spending Habit Tracker")
