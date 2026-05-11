8289278b67fce1d40e68a94ca9c8721dd8db64eb
Jacob0626 <jgonzalez8@caldwell.edu>
2026-05-11 02:10:17 +0000

    May 10, 2026, 10:10 PM

--- src/main.py
+++ src/main.py
@@ -292,7 +292,7 @@ elif page == "Budget Goals":
             step=10.0
         )
         
-        food_budget = st.number_input(
+        groceries_budget = st.number_input(
             "Food Budget",
             min_value=0.0,
             value=float(budget_settings["category_budgets"].get("Food", 0.0)),
