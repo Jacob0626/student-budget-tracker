a3a9b064c1c8a0508eb259661ecf6b3be59795bd
Jacob0626 <jgonzalez8@caldwell.edu>
2026-05-11 03:12:23 +0000

    May 10, 2026, 11:12 PM

--- src/main.py
+++ src/main.py
@@ -183,7 +183,7 @@ def create_summary_report(summary: dict, budget_settings: dict) -> pd.DataFrame:
             round(budget_settings["savings_goal"], 2)
         ]
     }
-    return 
+    return pd
 
 st.title("Student Budget & Spending Habit Tracker")
 
