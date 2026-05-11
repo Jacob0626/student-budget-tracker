c893b770a43bb36038a59302fcb32001e6cd1e43
Jacob0626 <jgonzalez8@caldwell.edu>
2026-05-11 02:32:02 +0000

    May 10, 2026, 10:32 PM

--- src/main.py
+++ src/main.py
@@ -134,7 +134,7 @@ def save_budget_settings(settings: dict) -> None:
         json.dump(settings, file, indent=4)
 
 def compare_budget_to_spending(df: pd.DataFrame, budget_settings: dict) -> pd.DataFrame:
-    """Com"""
+    """Compare category budgets to actual spending"""
 
 st.title("Student Budget & Spending Habit Tracker")
 
