596c69b287674b9f7ba5921b06a315b6423d19f2
Jacob0626 <jgonzalez8@caldwell.edu>
2026-05-11 02:17:38 +0000

    May 10, 2026, 10:17 PM

--- src/main.py
+++ src/main.py
@@ -328,7 +328,7 @@ elif page == "Budget Goals":
             }
             
             save_budget_settings(new_settings)
-            st.success
+            st.success("")
 
 
 elif page == "About":
